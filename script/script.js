536862058980f155e77e256fbf208f7b2eb2c16c
Eric Huang <ericwhhuang@gmail.com>
2019-03-08 23:52:45 -0800

    Completed basic design of market.html listing example. Updated script.js and market.css.

--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 
 //Marketplace.html 
 //Distance Slider
+
 var slider = document.getElementById("sliderBody");
 var output = document.getElementById("dist");
 output.innerHTML = slider.value;
@@ -12,21 +13,51 @@ slider.oninput = function() {
 //Results list to Grid sort view 
 $(function() {
   function list() {
-    $(".grid span").removeClass("active");
-    $(".list span").addClass("active");
+    $(".gridView span").removeClass("active");
+    $(".listView span").addClass("active");
     $(".wrapper")
     .removeClass("grid")
     .addClass("list");
+    
+    $(".itemImageWrapper").innerWidth("calc(150px - 28px)");
+    $(".itemImageWrapper").innerHeight("calc(150px - 28px)");
+    $(".itemText").innerWidth("calc(100% - 150px + 24px - 12px");
+
+    for (i = 0; i < 2; i++){
+      document.getElementsByClassName("itemImageWrapper")[i].style.display = "inline-block";
+      document.getElementsByClassName("itemImageWrapper")[i].style.margin = "0";
+      document.getElementsByClassName("itemText")[i].style.float = "right";
+      document.getElementsByClassName("itemDescription")[i].style.lineHeight = "1.15em";
+      document.getElementsByClassName("itemDescription")[i].style.height = "3.45em";
+      document.getElementsByClassName("itemHeader")[i].style.marginTop = "0px";
+      document.getElementsByClassName("itemDescription")[i].style.marginBottom = "0px";
+    }
   }
 
   function grid() {
-    $(".list span").removeClass("active");
-    $(".grid span").addClass("active");
+    $(".listView span").removeClass("active");
+    $(".gridView span").addClass("active");
     $(".wrapper")
     .removeClass("list")
     .addClass("grid");
+
+    $(".itemImageWrapper").innerWidth("140px");
+    $(".itemImageWrapper").innerHeight("140px");
+    $(".itemText").innerWidth("100%");
+
+    for (i = 0; i < 2; i++){
+      document.getElementsByClassName("itemImageWrapper")[i].style.display = "block";
+      document.getElementsByClassName("itemImageWrapper")[i].style.margin = "0 auto";
+      document.getElementsByClassName("itemText")[i].style.float = "none";
+      document.getElementsByClassName("itemDescription")[i].style.lineHeight = "1.15em";
+      document.getElementsByClassName("itemDescription")[i].style.height = "4.6em";
+      document.getElementsByClassName("itemHeader")[i].style.marginTop = "10px";
+      document.getElementsByClassName("itemDescription")[i].style.marginBottom = "10px";
+      document.getElementsByClassName("itemByUser")[i].style.innerHTML = "10px";
+      document.getElementsByClassName("itemPostedOn")[i].style.innerHTML = "10px";
+    }
   }
 
-  $(".list").click(list);
-  $(".grid").click(grid);
+  $(".listView").click(list);
+  $(".gridView").click(grid);
 });
