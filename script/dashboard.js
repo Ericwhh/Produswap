985bd563028fdf9a89dd7b3f3f85645f3375bd1e
Eric Huang <ericwhhuang@gmail.com>
2019-04-06 19:20:32 -0700

    Made tabs switchable in dashboard.html

--- script/dashboard.js
+++ script/dashboard.js
@@ -10,11 +10,33 @@ var usersRef = firebase.database().ref('users');
 var postsRef = firebase.database().ref('posts');
 console.log(postsRef.key);
 
-function offerDisplay(){
+$("#tabPosted").click(function(e){
+  postedDisplay();
+});
 
+$("#tabSent").click(function(e){
+  sentDisplay();
+});
+
+$("#tabReceived").click(function(e){
+  requestDisplay();
+});
+
+// Sets which tab to display
+function postedDisplay(){
+  $("#tradeBox").css("display", "block");
+  $("#sentBox").css("display", "none");
+  $("#receivedBox").css("display", "none");
+}
+function sentDisplay(){
+  $("#tradeBox").css("display", "none");
+  $("#sentBox").css("display", "block");
+  $("#receivedBox").css("display", "none");
 }
 function requestDisplay() {
-    
+  $("#tradeBox").css("display", "none");
+  $("#sentBox").css("display", "none");
+  $("#receivedBox").css("display", "block");
 }
 
 
@@ -30,26 +52,40 @@ $("#loginButton").click(function(){
 
 var currUser;
 // Gets the posts that belong to the current user
+// Gets the posts that the user has sent offers for
+// Gets the posts that the user has received offers for
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
       currUser = user.uid;
-      var currUserRef = firebase.database().ref("users/" + user.uid + "/posts");
-      currUserRef.once('value', function(snapshot){
+      var currUserPostsRef = firebase.database().ref("users/" + user.uid + "/posts");
+      currUserPostsRef.once('value', function(snapshot){
         snapshot.forEach(function(childSnapshot){
-          displayPost(childSnapshot.key);
+          displayPost(childSnapshot.key, "tradeBox");
+        });
+      });
+      var currUseSentRef = firebase.database().ref("users/" + user.uid + "/offersSent");
+      currUseSentRef.once('value', function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+          displayPost(childSnapshot.key, "sentBox");
+        });
+      });
+      var currUserReceivedRef = firebase.database().ref("users/" + user.uid + "/offersReceived");
+      currUserReceivedRef.once('value', function(snapshot){
+        snapshot.forEach(function(childSnapshot){
+          displayPost(childSnapshot.key, "receivedBox");
         });
       });
     }
 });
 
 // Displays the post with the ID.
-function displayPost(uniquePostID){
+function displayPost(uniquePostID, htmlID){
   var currentPostRef = firebase.database().ref("posts/" + uniquePostID);
   currentPostRef.once("value", function(snapshot){
     var obj = snapshot.val();
     var stringify = JSON.stringify(obj);
     var parse = JSON.parse(stringify);
-    addPostToPageListing("tradeBox", snapshot.key, parse.itemName, parse.category, parse.description,
+    addPostToPageListing(htmlID, snapshot.key, parse.itemName, parse.category, parse.description,
     parse.date, parse.email, parse.postedBy, parse.status, parse.imageLocation);
   });
 }
