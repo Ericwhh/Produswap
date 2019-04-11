e39a20c2838131dba60284daf039908655e77cfa
Eric Huang <ericwhhuang@gmail.com>
2019-04-10 21:18:38 -0700

    update

--- script/script.js
+++ script/script.js
@@ -1,15 +1,3 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAGVERfhd2wvN9zZPGRALe_hVLT3W_P0mg",
-  authDomain: "produswap.firebaseapp.com",
-  databaseURL: "https://produswap.firebaseio.com",
-  projectId: "produswap",
-  storageBucket: "produswap.appspot.com",
-  messagingSenderId: "645477140066"
-};
-firebase.initializeApp(config);
-
-
 //References for Firestorage
 const storage = firebase.storage()
 
